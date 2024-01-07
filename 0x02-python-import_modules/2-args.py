115b337cae95d5163ced4476ea975f8f9688ba08
hacker <hacker@kali.hacker>
2024-01-07 09:14:09 +0300

    task how to make a script dynamic corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-arg_length = len(sys.argv[1:])
-if arg_length == 1:
-    print(f"{arg_length} argument:")
-    print(f"1: {sys.argv[1]}")
-else:
-    print(f"{arg_length} arguments: ")
-    for i in range(1,arg_length):
-        print(f"{i}: {sys.argv[i]}")
+if __name__ == "__main__":
+    import sys
+    arg_length = len(sys.argv[1:])
+    if arg_length == 1:
+        print(f"{arg_length} argument:")
+        print(f"1: {sys.argv[1]}")
+    else:
+        print(f"{arg_length} arguments: ")
+        for i in range(1,arg_length):
+            print(f"{i}: {sys.argv[i]}")
