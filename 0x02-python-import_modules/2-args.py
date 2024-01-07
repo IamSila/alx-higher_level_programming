247d8c1e0414dbf18fb71a2fd1bbe4204a0cfeeb
hacker <hacker@kali.hacker>
2024-01-07 09:27:12 +0300

    pycodestyle is upto the task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,11 @@
 if __name__ == "__main__":
     import sys
     args = len(sys.argv)
-    
-    if(args <= 1):
+    if (args <= 1):
         print("{} arguments.".format(args - 1))
-    elif(args == 2):
+    elif (args == 2):
         print("{} argument:".format(args - 1))
     else:
         print("{} arguments:".format(args - 1))
     for x in range(1, args):
-        print("{}: {}".format(x, sys.argv[x]))    
+        print("{}: {}".format(x, sys.argv[x]))
