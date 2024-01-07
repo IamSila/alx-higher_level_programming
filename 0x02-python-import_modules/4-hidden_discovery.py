618e6b95ec10e07b45616b1e04a46c54dcf99d90
IamSila <silamulingi@gmail.com>
2024-01-06 23:51:27 -0800

    task 4 who are you corrections

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4 as hidden_file
     list_of_names = dir(hidden_file)
-    for x in range(len(hidden_file)):
+    for x in range(len(list_of_names)):
         if (list_of_names[x][0]) != '_':
             print(list_of_names[x])
