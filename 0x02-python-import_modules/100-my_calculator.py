36058a82fa739081a00b82f08acc8ff29bee9473
hacker <hacker@kali.hacker>
2024-01-07 14:21:29 +0300

    Build my own calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    a = int(sys.argv[1])
+    b = (int(sys.argv[3]))
+    operator = sys.argv[-2]
+    operations = {
+        "+": add,
+        "-":sub,
+        "*":mul,
+        "/": div
+        }
+    argument_len = len(sys.argv)
+    if (argument_len - 1) != 3:
+        sys.stderr.write("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+        sys.exit(1)
+    
+    elif (sys.argv[2]) not in operations.keys():
+         print("Unknown operator. Available operators: +, -, * and /")
+
+    else:
+        print("{} {} {} = {}".format(a, operator, b, operations[sys.argv[2]](a, b)))
