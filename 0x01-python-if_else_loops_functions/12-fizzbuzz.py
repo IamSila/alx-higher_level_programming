8a9948325ab9760a80847bf85d8de4041004be9b
hacker <hacker@kali.hacker>
2024-01-10 01:43:28 +0300

    fizz fuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-for number in range(1, 101):
-    if ((number % 3 == 0) and (number % 5 == 0)):
-        number = "FizzBuzz"
-    elif number % 3 == 0:
-        number = "Fizz"
-    elif number % 5 == 0:
-        number = "Buzz"
-    else:
-        number = number
-    print("{}".format(number), end=' ')
+def fizzbuzz():
+    for number in range(1, 101):
+        if ((number % 3 == 0) and (number % 5 == 0)):
+            number = "FizzBuzz"
+        elif number % 3 == 0:
+            number = "Fizz"
+        elif number % 5 == 0:
+            number = "Buzz"
+        else:
+            number
+        print("{}".format(number), end=' ')
