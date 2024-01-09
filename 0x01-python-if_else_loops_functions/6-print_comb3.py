f99c30918242919cc4dc53bfffdad998e19c1425
hacker <hacker@kali.hacker>
2024-01-09 14:28:56 +0300

    all combinations of two numbers where the numbers cannot be same and made it an executable

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for a in range(0, 10, 1):
     for b in range((a+1), 10, 1):
         if (a != 8):
             print("{}{}, ".format(a, b), end='')
-        else:    
+        else:
             print("{}{}".format(a, b))
