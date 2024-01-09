cf998726bf838cb97cb1bbacf1bda66d1b4cd5ac
hacker <hacker@kali.hacker>
2024-01-09 14:20:16 +0300

    all combinations of two numbers where the numbers cannot be same

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for a in range(0, 10, 1):
-    for b in range(a+1, 10, 1):
-        if (a == 8) and (b == 9):
-            print("{}{}".format(a, b))
-        else:    
+    for b in range((a+1), 10, 1):
+        if (a is not 8):
             print("{}{}, ".format(a, b), end='')
+        else:    
+            print("{}{}".format(a, b))
