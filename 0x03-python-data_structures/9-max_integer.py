a7d6eee1ff1b241f13af17983b0905e575e2f50c
hacker <hacker@kali.hacker>
2024-01-12 17:39:25 +0300

    finding the max number in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def max_integer(my_list):
-    if my_list is not None:
-        my_list.sort()
-        maximum = my_list[len(my_list)-1]
-    return maximum
+    if my_list:
+        largest = my_list[0]
+        for i in my_list:
+            if i > largest:
+                largest = i
+        return largest
+    else:
+        return None
