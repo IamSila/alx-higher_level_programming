90c3fa2b67edf93fb6a9b7da1f9745cf670b249e
hacker <hacker@kali.hacker>
2024-01-12 13:49:18 +0300

    removing an element from a string using index slicing

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    for x in range(0, len(my_string), 1):
+        if my_string[x] == 'c' or my_string[x] == 'C':
+            return my_string[:x] + my_string[x+1:]
