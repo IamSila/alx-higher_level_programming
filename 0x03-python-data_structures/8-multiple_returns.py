37bc710f03b020f2943786c3b798e369dfed5559
hacker <hacker@kali.hacker>
2024-01-12 16:50:50 +0300

    length and first charecter

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,6 @@ def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
     if length == "\0":
-        length = None
-        return length, first
+        return None, first
     else:
         return length, first
