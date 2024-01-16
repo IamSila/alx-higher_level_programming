28c8f4e9fa9b66bd46e5b1c7cc408eaacc35bb6e
hacker <hacker@kali.hacker>
2024-01-16 11:24:17 +0300

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    print(a_dictionary)
+
+
+def print_sorted_dictionary(dictionary):
+    print(dictionary)
