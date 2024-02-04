57d73ba7228bfff936d4de8bc283718db201a4d3
hacker <hacker@kali.hacker>
2024-02-04 21:38:22 +0300

    a class with a private variable

--- 0x06-python-classes/0-main.py
+++ 0x06-python-classes/0-main.py
@@ -2,6 +2,6 @@
 
 square = __import__('0-square').Square
 
-my_square = Square()
+my_square = square()
 print(type(my_square))
 print(my_square.__dict__)
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""This is a class called Square which will have attributes of a square"""
+
+
+class Square:
+
+    """method to initialize the class """
+
+    def __init__(self, __size):
+        """The variable size is a private variable"""
+        self.__size = __size
