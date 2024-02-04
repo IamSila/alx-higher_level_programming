8c757c2530708581cf314c406118c29bd06b7c7c
hacker <hacker@kali.hacker>
2024-02-04 22:58:55 +0300

    defining a method that checks for some values

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,7 +3,13 @@
 
 
 class Square:
-    """This method initializes the class"""
+    """This method initializes the class
+    
+    Initialize a Square instance with a given size.
+
+        Args:
+            size (int): The size of the square (default is 0).
+    """
     def __init__(self, __size=0):
         self.__size = __size
 
