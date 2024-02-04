eaab3ede85c7b52f82102e81994d3298e6e41ba3
hacker <hacker@kali.hacker>
2024-02-04 22:53:45 +0300

    defining a method that checks for some values

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,9 +7,10 @@ class Square:
     def __init__(self, __size=0):
         self.__size = __size
 
-    def checker(self, __size):
-        """This class checks if size is a int and if size is greater than 0"""
-        if (self.__size) != int:
+        if not isinstance(self.__size, int):
             raise TypeError("size must be an integer")
         elif self.__size < 0:
             raise ValueError("size must be >= 0")
+
+    
+        
