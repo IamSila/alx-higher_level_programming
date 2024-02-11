8986dbcd3bae57c9a4b77dee7b28cc010f9bd7d2
hacker <hacker@kali.hacker>
2024-02-12 01:36:57 +0300

    printing a user's first and last name or first name only

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def say_my_name(first_name, last_name=""):
-    if type(first_name) not in [str]:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if type(last_name) not in [str]:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
