ce660fd3f09887de3fb6148f348b64a78937f9f3
hacker <hacker@kali.hacker>
2024-02-14 08:47:20 +0300

    print a square using # symbol

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    this script prints a square interms of hashes
+    depending on size of the square
+"""
+def print_square(size):
+    """
+        prints a square shape
+        args:
+            size: the length of the square
+    """
+    #size must be an integer
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    
+    for distance in range(0, size, 1):
+        print("#" * size)
