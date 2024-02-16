18a90694e2d80807330107d8a9a0f843f47ee29b
hacker <hacker@kali.hacker>
2024-02-16 22:29:04 +0300

    a class rectangle with width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,7 +2,7 @@
 """Rectangle class with rectangle attributes"""
 
 
-class Rectangle:
+class Rectangle():
     """Has xtics of a rectangle"""
     def __init__(self, width=0, height=0):
         """Instantiation with optional width and height"""
@@ -23,6 +23,7 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
+
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
@@ -41,6 +42,7 @@ class Rectangle:
         """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
+
         if value < 0:
             raise ValueError("height must be an integer")
         self.__height = value
