f6cc7a8c0c24363a0d854b382adbd5af0cea26f0
hacker <hacker@kali.hacker>
2024-02-26 16:20:46 +0300

    linked lists

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -9,7 +9,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * for Holberton project
  */
 typedef struct listint_s
 {
@@ -18,9 +18,8 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
-listint_t *insert_node(listint_t **head, int number);
+int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
