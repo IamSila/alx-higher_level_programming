490fd190fa681de8dcd6469b2dd4403cd888667c
hacker <hacker@kali.hacker>
2024-02-27 16:07:12 +0300

    weighted evarage

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,10 +4,10 @@
 def weight_average(my_list=[]):
     numerator = 0
     denominator = 0
+    evarage = numerator / denominator
 
     for elements in my_list:
         numerator += (elements[0] * elements[1])
         denominator += elements[1]
 
-    evarage = numerator / denominator
     return evarage
