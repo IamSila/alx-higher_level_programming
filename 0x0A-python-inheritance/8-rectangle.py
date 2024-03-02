cfc2ba3f3763426674e46d142925986c00167161
hacker <hacker@kali.hacker>
2024-03-02 12:50:16 +0300

    inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""a child class that defines a rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """class that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Intialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
