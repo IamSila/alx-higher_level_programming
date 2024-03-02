c036fc5aa43f94002fb6d8954eac601a3e15075b
hacker <hacker@kali.hacker>
2024-03-02 09:04:25 +0300

    inheriting from list built in class

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,9 +8,6 @@ class MyList(list):
         args:
             list - holds the unsorted list
     """
-    def __init__(self):
-        """initializing the object"""
-        super().__init__()
     def print_sorted(self):
         """prints a sorted list"""
         print(sorted(self))
