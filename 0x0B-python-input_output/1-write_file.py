01d88b1eb00e9e9d004005bafa3e44a9828053c0
hacker <hacker@kali.hacker>
2024-03-04 14:19:04 +0300

    writing to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""function called write_file
+    args:
+        filename - the file to write
+        text - the text to write to the file          
+"""
+
+def write_file(filename="", text=""):
+    """
+    open a file named filename and write the contents
+    of text to the file
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
+
