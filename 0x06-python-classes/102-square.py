0f2385ffc0db1c907b1de5770863e9437e8b4d03
hacker <hacker@kali.hacker>
2024-03-05 10:09:49 +0300

    comparing squares using more class methods

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Create a Class Square with size, method of area and getters & setters"""
+
+
+class Square:
+    """Class - Square"""
+
+    def __init__(self, size=0):
+        """Constructor of a Square with the size"""
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
+
+    def area(self):
+        """Method to get the area of the Square"""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """Getter of the private attribute size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size private attribute"""
+        if (type(value) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (value < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = value
+
+    def __eq__(self, other):
+        """Define the == comparision to a Square."""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """Define the != comparison to a Square."""
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        """Define the < comparison to a Square."""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """Define the <= comparison to a Square."""
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """Define the > comparison to a Square."""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """Define the >= compmarison to a Square."""
+        return self.area() >= other.area()
