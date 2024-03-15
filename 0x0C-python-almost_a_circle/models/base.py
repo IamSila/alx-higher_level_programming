82d480f007b20da0ae19f9917137d352274f2e35
IamSila <silamulingi@gmail.com>
2024-03-15 20:35:49 +0300

    almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,4 +15,5 @@ class Base(object):
         if self.id is not None:
             self.id = id
         else:
-            self.id = self.__nb_objects = + 1
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
