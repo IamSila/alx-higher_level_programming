bab64a36e59cc07892a0b31031cf41be3790b1c1
IamSila <silamulingi@gmail.com>
2024-03-15 22:43:28 +0300

    The rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a rectangle class."""
+from models.base import Base
 
 class Rectangle(Base):
     """Represent a rectangle."""
@@ -27,10 +28,6 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -40,10 +37,6 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -53,10 +46,6 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if type(value) != int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -66,8 +55,4 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if type(value) != int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
         self.__y = value
