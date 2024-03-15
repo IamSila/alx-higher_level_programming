4b23f295defe6de61e0c744eab1e299d6181cfb9
IamSila <silamulingi@gmail.com>
2024-03-15 20:29:14 +0300

    almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""This is a class called base class to act as the parent class"""
+
+
+class Base(object):
+    """Defining the attributes of the class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        initializing the class attributes
+        """
+        if self.id is not None:
+            self.id = id
+        else:
+            self.id = Base.__nb_objects = + 1
