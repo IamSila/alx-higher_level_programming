70b3bc97451710e130dc8def5762ada723a37f4e
IamSila <silamulingi@gmail.com>
2024-03-15 22:22:23 +0300

    The rectangle class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+"""defines a Rectangle class."""
+
+
+class Rectangle(Base):
+    """
+    Rectangle model.
+
+    This class inherits from the Base class
+
+    Private instance Attributes:
+       __width (int): The width of the rectangle.
+       __height (int): The height of the rectangle
+       x -
+       y -
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+
+        Args:
+            width(int): The width of the rectangle
+            height - height of the rectangle
+            x - x of the rectangle
+            y - y of the rectangle
+            id - is the id of the rectangle
+        """
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+        @property
+        def width(self):
+            """width of the rectangle"""
+            return self.__width
+
+        @property
+        def height(self):
+            """
+            height of the rectangle
+            """
+            return self.__height
+
+        @property
+        def x(self):
+            """x of the rectangle"""
+            return self.__x
+
+        @property
+        def y(self):
+            """y of the rectangle"""
+            return self.__y
+
+        @width.setter
+        def width(self, value):
+            """set width = value"""
+            self.__width = value
+
+        @height.setter
+        def height(self, value):
+            """set height = value"""
+            self.__height = value
+
+        @x.setter
+        def x(self, value):
+            """set x = value"""
+            self.__x = value
+
+        @width.setter
+        def y(self, value):
+            """set y = value"""
+            self.__y = value
