cf16951e4bc93d945416366491501df93565891b
IamSila <silamulingi@gmail.com>
2024-03-16 13:30:07 +0300

    a class square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""a class square"""
+"""Defines a square class."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represents a square"""
+    """Represent a square."""
+
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a new Square.with the following arguments
+        """Initialize a new Square.
 
         Args:
             size (int): The size of the new Square.
@@ -14,8 +15,7 @@ class Square(Rectangle):
             y (int): The y coordinate of the new Square.
             id (int): The identity of the new Square.
         """
-
-        super()._init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """
