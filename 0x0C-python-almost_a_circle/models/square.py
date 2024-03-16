6297e12e74ade5cef14bc47c2d8ad1d7bb6734ca
IamSila <silamulingi@gmail.com>
2024-03-16 14:00:35 +0300

    added an update method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,6 +29,17 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
+        """Update the Square.
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+
         if args and len(args) != 0:
             a = 0
             for arg in args:
