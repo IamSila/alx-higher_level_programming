1ebb75d167c0a8f20a3d47e373ef87d81a96c1aa
IamSila <silamulingi@gmail.com>
2024-03-16 13:27:18 +0300

    a class square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,15 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Represents a square"""
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square.with the following arguments
+
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
+
         super()._init__(size, size, x, y, id)
         self.width = size
         self.height = size
