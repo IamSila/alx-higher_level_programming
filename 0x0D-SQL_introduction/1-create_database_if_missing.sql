1cb410f1ea61636e95518e8515262041436a9ffe
IamSila <silamulingi@gmail.com>
2024-07-05 10:58:53 +0300

    pushing the directory

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,4 @@
---create a database if it is missing
-CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
+-- Creates the database hbtn_0c_0.
+-- script should not give an error if it is missing
+
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`; --create a database
