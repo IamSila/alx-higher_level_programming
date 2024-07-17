cb71e6fb26f264e746fb66197f13be44a2657146
IamSila <silamulingi@gmail.com>
2024-07-17 13:58:43 +0300

    grant, creating user, and flushing privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,10 +1,11 @@
 --create a user identified by the given pass and pass all privileges on the server 
 
 
-CREATE USER 
-	if NOT EXISTS 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL privileges
-	ON *.* TO 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
