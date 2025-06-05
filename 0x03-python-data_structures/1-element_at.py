1ecf44875cd09a621420649925c114864fcce9b7
IamSila <silamulingi@gmail.com>
2025-06-05 22:45:15 +0300

    getting an element at and index position x

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,9 +3,10 @@
 
 def element_at(my_list, idx: int):
     """returns an element at index position specified"""
+    length = len(my_list)
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx > length:
         return None
     else:
         return my_list[idx]
