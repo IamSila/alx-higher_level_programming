0828b242e9e36b6504058f20cf67e111c487c435
IamSila <silamulingi@gmail.com>
2025-06-05 21:22:10 +0300

    edited the file alittle

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,4 +4,4 @@
 def print_list_integer(my_list=[]):
     """This function returns all the ints inside a list"""
     for integer in my_list:
-        print(integer)
+        print("{:d}".format(integer))
