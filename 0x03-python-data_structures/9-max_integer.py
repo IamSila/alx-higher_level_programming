3748d29614e06a2b4b0688d2f2c3b88382067076
IamSila <silamulingi@gmail.com>
2025-06-06 11:00:10 +0300

    max value in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,10 +3,12 @@
 
 def max_integer(my_list=[]):
     """returns the max value inside a list"""
-    length = len(my_list)
-    if length > 0:
-        for i in range(0, length - 1):
-            if my_list[i] > my_list[i + 1]:
-                return my_list[i]
+
+    if my_list:
+        largest = my_list[0]
+        for i in my_list:
+            if i > largest:
+                largest = i
+        return largest
     else:
         return None
