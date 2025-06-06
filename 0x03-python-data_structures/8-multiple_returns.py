2ac7d8068b1691a2420c47f460b21e26ce067a02
IamSila <silamulingi@gmail.com>
2025-06-06 10:45:23 +0300

    multiple returns from a single character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 
 def multiple_returns(sentence):
     """returns two value the length and the first character"""
-    if sentence:
-        length = len(sentence)
-        first = sentence[0] if length > 0 else None
-        return length, first
+
+    length = len(sentence)
+    first = sentence[0] if length > 0 else None
+    return length, first
