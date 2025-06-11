40fc5290f5535c17da66960ed465fb14b650b64b
IamSila <silamulingi@gmail.com>
2025-06-11 11:09:50 +0300

    squaring all values in a dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,6 +4,8 @@
 def multiply_by_2(a_dictionary):
     """returns a new dictionary with all values multiplied by 2"""
     new_dictionary = a_dictionary.copy()
-    for key, value in new_dictionary.items():
+    new_dictionaryKeys = list(new_dictionary)
+
+    for key in new_dictionaryKeys:
         new_dictionary[key] = value**2
     return new_dictionary
