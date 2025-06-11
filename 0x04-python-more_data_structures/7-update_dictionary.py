3ca3baab41e5b6e54233c1b234e68bec2d71939f
IamSila <silamulingi@gmail.com>
2025-06-11 10:37:48 +0300

    adding/ replacing a key/vale pair in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key: str, value: any):
+    """adds or replaces key/vale pair in a dictionary"""
+    a_dictionary[key] = value
+    return a_dictionary
