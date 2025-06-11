bf7e5f9de3cadafce9d22d013dc77b2dee6fb2ad
IamSila <silamulingi@gmail.com>
2025-06-11 11:21:08 +0300

    squaring all values corrected

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -6,6 +6,6 @@ def multiply_by_2(a_dictionary):
     new_dictionary = a_dictionary.copy()
     new_dictionaryKeys = list(new_dictionary.keys())
 
-    for key in new_dictionaryKeys:
-        new_dictionary[key] *= 2
+    for key, value in new_dictionary.items():
+        new_dictionary[key] = value * 2
     return new_dictionary
