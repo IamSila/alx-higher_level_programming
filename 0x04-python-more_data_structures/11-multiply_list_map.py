d3722fea4dda126928e01534a1a991e91e151fa7
IamSila <silamulingi@gmail.com>
2025-06-13 16:17:05 +0300

    multiplying all values in a list with a given number

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -2,5 +2,6 @@
 
 
 def multiply_list_map(my_list=[], number=0):
-    """returns all values in a list  multiplied by a number without using a loop"""
+    """returns all values in a list in
+    multiplied by a number without using a loop"""
     return list(map(lambda x: x * number, my_list))
