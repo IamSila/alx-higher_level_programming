90bb6055403e9d92b8d1e8d1b7153f9bcffee012
IamSila <silamulingi@gmail.com>
2025-06-14 21:15:45 +0300

    weighted average--code correction

--- 0x04-python-more_data_structures/100-weight_evarage.py
+++ 0x04-python-more_data_structures/100-weight_evarage.py
@@ -3,11 +3,15 @@
 
 def weight_evarage(my_list=[]):
     """calculates the weighted evarage of values in a tuple"""
+
     result = 0
     total = 0
-    if len(my_list) == 0:
+
+    if not my_list:
         return 0
+
     for i in my_list:
         result += i[0] * i[1]
         total += i[1]
-    return result / total
+    weightedAverage = result / total
+    return weightedAverage
