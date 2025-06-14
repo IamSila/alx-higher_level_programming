bf2e78efd61b380fa86e5f54748cbedf5efbedfb
IamSila <silamulingi@gmail.com>
2025-06-14 21:48:11 +0300

    squares of a matrix using map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -5,6 +5,4 @@ def square_matrix_map(matrix=[]):
     """return squared values of a matrix"""
     new_matrix = matrix.copy()
 
-    for row in range(len(new_matrix)):
-        new_matrix[row] = list(map(lambda x: x**2, new_matrix[row]))
-    return new_matrix
+    return list(map(lambda y: list(map(lambda x: x**2, y)), matrix))
