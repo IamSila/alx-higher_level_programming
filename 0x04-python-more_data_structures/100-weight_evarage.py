74a6d0e102b4fba70569a8d778c13147e60aa96b
IamSila <silamulingi@gmail.com>
2025-06-14 21:25:10 +0300

    weighted average--code correction

--- 0x04-python-more_data_structures/100-weight_evarage.py
+++ 0x04-python-more_data_structures/100-weight_evarage.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-
-
 def weight_average(my_list=[]):
     if not my_list:
         return 0
 
-    numerator = 0
-    denominator = 0
+    num = 0
+    den = 0
 
-    for elements in my_list:
-        numerator += elements[0] * elements[1]
-        denominator += elements[1]
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
 
-    evarage = numerator / denominator
-    return evarage
+    return num / den
