d3eed56a5e34dd2da0451060f922dfdbc15dcd32
IamSila <silamulingi@gmail.com>
2025-06-16 12:15:50 +0300

    complex delete

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -5,10 +5,10 @@ dictionary = {"lang": "C", "track": "Low", "pref": "C", "ids": [1, 2, 3]}
 def complex_delete(a_dictionary, value):
     if value not in a_dictionary.values():
         return a_dictionary
-    dictionary_keys = list(a_dictionary)
-    for i in dictionary_keys:
-        if a_dictionary[i] == value:
-            del a_dictionary[i]
+    dictionary_keys = list(a_dictionary.keys())
+    for key in dictionary_keys:
+        if value == a_dictionary.get(key):
+            del a_dictionary[key]
     return a_dictionary
 
 
