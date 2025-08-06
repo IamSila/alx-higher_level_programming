24bcb009e875b78efdb06e49d88c31cf85cece91
IamSila <silamulingi@gmail.com>
2025-08-07 00:00:18 +0300

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!usr/bin/node
+// Rectangle Class with validation for width and height
+
+class Rectangle {
+  constructor (w, h) {
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    console.log(('X'.repeat(this.width) + '\n').repeat(this.height).split('')
+      .slice(0, -1).join(''));
+  }
+}
+module.exports = Rectangle;
